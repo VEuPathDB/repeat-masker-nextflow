2bf9214af482a20c02224538f32cb0840eb7ee02
rdemko2332 <rdemko2332@gmail.com>
2022-06-28 14:30:08 -0400

    Updated seqcleaner.pl

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,13 @@ FROM dfam/tetools:1.5
 
 MAINTAINER rdemko2332@gmail.com
 
+RUN apt-get -qq update --fix-missing
+
+RUN apt-get install -y \
+  cpanminus \
+  bioperl \
+  bioperl-run
+
 COPY ./bin/seqCleaner.pl /usr/local/bin/seqCleaner.pl
 
 RUN chmod +x /usr/local/bin/seqCleaner.pl
--- bin/seqCleaner.pl
+++ bin/seqCleaner.pl
@@ -3,6 +3,8 @@
 use strict;
 use FileHandle;
 use Getopt::Long;
+use Bio::SeqIO;
+use IO::String;
 
 my($inFile,$outFile,$errFile,$trimDangling,$dangleMax,$paramFile);
 &GetOptions('seqFile=s' => \$inFile,
@@ -14,88 +16,40 @@ my($inFile,$outFile,$errFile,$trimDangling,$dangleMax,$paramFile);
 my $errorFile = $errFile ? $errFile : "error.log";
 
 open(ERR, ">$errorFile") || die "Could not open error file $errorFile";
-select ERR; $| = 1;
-select STDOUT;
-$| = 1;
   
 open(OUT, ">$outFile") || die "Could not open outFile $outFile";
 
-my $tmpSeq = "";
-my $miniLib = "";
-my $countNumSeqs = 0;
-my $ctLength = 0;
-my $countPart = 0;
+my $in  = Bio::SeqIO->new(-file => $inFile, -format => 'Fasta');
+my $out = Bio::SeqIO->new(-file => ">$outFile", -format => 'Fasta');
 
-##generate better sequence....
-my @qmSeq = &readSeqFileToArray($inFile);
-foreach my $seq (@qmSeq) {
-    $seq->[1] =~ s/\s+//g;##gets rid of spaces and newlines
-  
-    my $sequence = $trimDangling ? &trimDanglingNNN($seq->[1]) : $seq->[1];
-  
-    my $tmpSeq = $sequence;
-    $tmpSeq =~ s/N//g;
-  
+while ( my $seq = $in->next_seq() ) {
+    my $sequenceTrimmed = $trimDangling ? &trimDanglingNNN($seq->seq) : $seq->seq;
+    my $tmpSeq = $sequenceTrimmed;
+    $tmpSeq =~ s/N//g;  
     if (length($tmpSeq) > 50) {
-	print OUT "$seq->[0]", &breakSequence($sequence);
+        my $new = "";
+        print OUT "\>" . $seq->id . "\n";
+        for (my $i = 0;$i<length($sequenceTrimmed);$i+=80) {
+            $new = substr($sequenceTrimmed,$i,80) . "\n";
+            print OUT $new;
+        }
     } else {
-	my $l = length($tmpSeq);
-	my $def = $seq->[0];
-	chomp $def;
-	print ERR "$def [TOO SHORT: $l]\n";
+	print ERR $seq->id . "[TOO SHORT: length($tmpSeq)]\n";
     }
-}
-
-close ERR;
-close OUT;
-
-sub breakSequence {
-    my($seq) = @_;
-  ##just in case there are returns...
-    $seq =~ s/\s//g;
-    my $new = "";
-    for (my $i = 0;$i<length($seq);$i+=80) {
-	$new .= substr($seq,$i,80) . "\n";
-    }
-    return $new;
-}
-
-sub readSeqFileToArray{##read into array or arrays
-    my($file) = @_;
-    my @seq;
-    my $c = 0-1;
-    my $fh = FileHandle->new($file);
-    open(TF, "$file");
-    while (<TF>) {
-	if (/^\>/) {
-	    $c++;
-	    $seq[$c]->[0] = $_;##makes key the entire defline...
-	} else {
-	    $seq[$c]->[1] .= $_; 
-	}
-    }
-    close TF;
-    return @seq;
 }
 
 sub trimDanglingNNN {
     my ($seq) = @_;
-
-
-  # forward strand
     my $mightNeedTrimming = 1;
     while($mightNeedTrimming) {
 	($seq, $mightNeedTrimming) = &trimDanglingNNN_sub($seq);
     }
-
-  # reverse strand (only bother if there are and NNNs left)
     if ($seq =~ /N/) {
 	my $rev = &reverseSequence($seq);
 	$mightNeedTrimming = 1;
 	while($mightNeedTrimming) {
 	    ($rev, $mightNeedTrimming) = &trimDanglingNNN_sub($rev);
 	}
-    # don't bother unreversing if not changed
 	if (length($rev) != length($seq)) {
 	    $seq = &reverseSequence($rev);
 	}
@@ -103,11 +57,8 @@ sub trimDanglingNNN {
     return $seq;
 }
 
-
-
 sub trimDanglingNNN_sub {
     my($seq) = @_;
-
     my $trimmed; 
     if ($seq =~ /^(.*?)NNNNNNNNNN+(.*?)$/) {
 	if (length($1) <= $dangleMax) {
@@ -125,4 +76,3 @@ sub reverseSequence{
     my @revseq = split('', $revseq);
     return $revseq;
 }
-
