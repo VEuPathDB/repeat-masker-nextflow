edb6326b0ed85bf90b8b3f0d82eb97091ddb74c5
rdemko2332 <rdemko2332@gmail.com>
2023-02-14 11:29:09 -0500

    Removing unneeded parameter

--- modules/templates/runRepeatMasker.bash
+++ modules/templates/runRepeatMasker.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-export LIBDIR=$params.libraryPath
+export LIBDIR=/opt/RepeatMasker/Libraries
 RepeatMasker $params.rmParams $subsetFasta -dir .
 if ! [-f "subset.fa.masked"]
 then
