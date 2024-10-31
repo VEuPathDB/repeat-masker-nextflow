00ab0819a65c7aa20e85617cff3280c73bfc6ddc
rdemko2332 <rdemko2332@gmail.com>
2024-10-31 16:02:06 -0400

    Indexing by 0 for bed files

--- modules/templates/runRepeatMasker.bash
+++ modules/templates/runRepeatMasker.bash
@@ -7,4 +7,8 @@ if [ ! -f \$expectedMaskedFile ]; then
     touch ${subsetFasta}.out.gff
 fi
 sed -i 1,2d ${subsetFasta}.out.gff
-cut -f 1,4,5 ${subsetFasta}.out.gff > ${subsetFasta}.bed
+cut -f 1,4,5 ${subsetFasta}.out.gff > rows
+awk '\$2--{print}' rows > ${subsetFasta}.bed
+sed -i 's/\s/\t/g' ${subsetFasta}.bed
+
+
