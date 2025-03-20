0f5d7b60951fb1744224937904b7f2984a0acdbd
rdemko2332 <rdemko2332@gmail.com>
2025-03-20 15:39:51 -0400

    Updating gff file to have original name

--- modules/templates/runRepeatMasker.bash
+++ modules/templates/runRepeatMasker.bash
@@ -16,6 +16,10 @@ else
 	--inFasta updatedFasta.fa \
 	--mappingFile mapping.tsv \
 	--restoredFasta ${subsetFasta}.masked
+    updateGff.pl \
+	 --inGff updatedFasta.fa.out.gff \
+	 --mappingFile mapping.tsv \
+	 --newGff ${subsetFasta}.out.gff
 fi
 sed -i 1,2d ${subsetFasta}.out.gff
 cut -f 1,4,5 ${subsetFasta}.out.gff > rows
